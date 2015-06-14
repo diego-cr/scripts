aed9b424836acc7339769c1f835350eabfe4c887
diego-cr <diegocr@gmail.com>
2015-06-14 20:28:27 +0200

    Update chagallwifi.sh

--- chagallwifi.sh
+++ chagallwifi.sh
@@ -5,16 +5,6 @@ res1=$(date +%s.%N)
 
 sed -i -e 's/ezio84/diegocr/g' vendor/slim/config/common.mk
 
-echo
-#echo "change cm to slim"
-#cp  device/samsung/chagallwifi/cm.mk device/samsung/chagallwifi/slim.mk
-#cp  device/samsung/chagallwifi/cm.dependencies device/samsung/chagallwifi/slim.dependencies
-#sed -i -e 's/cm/slim/g' device/samsung/chagallwifi/slim.mk
-
-#echo
-#echo "remove overlays"
-#sed -i '/config_deviceHardwareWakeKeys/d' device/samsung/chagallwifi/overlay/frameworks/base/core/res/res/values/config.xml
-#rm device/samsung/chagallwifi/overlay/frameworks/base/core/res/res/values/cm_arrays.xml
 
 echo
 echo "patch media codecs"
@@ -26,19 +16,9 @@ export USE_CCACHE=1
 
 echo
 echo building
-#. build/envsetup.sh
-#breakfast chagallwifi
-#brunch chagallwifi
 
-#./build_rom.sh chagallwifi nosync 5 noclean
 ./build_rom.sh chagallwifi nosync 5 clean
 
-echo
-echo "repacking..."
-echo
-#cd repackrom
-#./repack.sh ../out/target/product/chagallwifi/Slim.diegocr*.zip 
-#cd ..
 mv ./out/target/product/chagallwifi/Slim.diegocr* ./1SlimLP
 
 # Get elapsed time
