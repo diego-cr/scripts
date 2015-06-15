4f91ad42befb154d1ca924f867688bc82168c46c
diego-cr <diegocr@gmail.com>
2015-06-15 09:35:11 +0200

    Update chagallwifi.sh

--- chagallwifi.sh
+++ chagallwifi.sh
@@ -1,13 +1,6 @@
 #!/bin/bash
 sed -i -e 's/ezio84/diegocr/g' vendor/slim/config/common.mk
 
-export IS_RELEASED_BUILD=true
-export USE_CCACHE=1
-/usr/bin/ccache -M 50G
-
-echo
-echo building
-
 ./build_rom.sh chagallwifi nosync 5 clean
 
 mv ./out/target/product/chagallwifi/Slim.diegocr* ./1SlimLP
